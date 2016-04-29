6217e776cdda442e7b53bef19a6768171d71a7e3
shimaz <shimaz.bsh@gmail.com>
2016-04-29 19:04:59 +0900

    Added DrawView

--- app/src/main/java/kr/tangomike/artspectrum2016/MainActivity.java
+++ app/src/main/java/kr/tangomike/artspectrum2016/MainActivity.java
@@ -1,13 +1,28 @@
 package kr.tangomike.artspectrum2016;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.view.ViewGroup.LayoutParams;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout rlMain;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rlMain = (RelativeLayout)findViewById(R.id.rl_main);
+
+        DrawView dv = new DrawView(this);
+        dv.setLayoutParams(new LayoutParams(300, 300));
+
+
+
+
     }
 }
